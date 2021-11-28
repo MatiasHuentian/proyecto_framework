2d6356e23741c4a4d03fe6159b284453477c77dd
Mariany Lovera <mariany.lovera.sanchez@ciisa.cl>
2021-11-28 01:33:58 -0300

    Diseño de vistas de agregar y editar

--- resources/views/pages/products/create.blade.php
+++ resources/views/pages/products/create.blade.php
@@ -4,49 +4,54 @@
 
 @section('content')
     @section('titulo', 'Agregar')
+
+    <h1 class="text-center">Agregar Producto</h1>
+    <br>
+
         <div class="card mx-auto" style="width: 40rem;"  >
             <div class="card-body">
                 <form action="{{url('/productos/store')}}" method="POST" class="row g-2">
                     @csrf 
-                    <div class="col-md-4" >
-                        <label for="codigo" class="form-label">Código unico</label>
+                    <div class="form-group col-md-6" >
+                        <label for="codigo" class="form-label">Código único</label>
                         <input type="codigo" class="form-control" id="codigo" name="codigo">
                     </div>
-                    <div class="col-md-4">
+                    <div class="form-group col-md-6">
                         <label for="nomProducto" class="form-label">Nombre Producto</label>
                         <input type="nomProducto" class="form-control" id="nomProducto" name="name">
                     </div>
-                    <div class="col-md-4" >
+                    <div class="form-group col-md-6" >
                         <label for="precio" class="form-label">Precio venta</label>
                         <input type="precio" class="form-control" id="precio" name="precio_venta">
                     </div>
-                    <div class="col-md-4">
+                    <div class="form-group col-md-6">
                         <label for="nom" class="form-label">Categoria</label>
-                        <select class="form-select " name="nom_categoria">
+                        <select class="form-control " name="nom_categoria">
                             <option selected>Categoria</option>
                             @foreach ($categories as $category)
                                 <option value="{{ $category->id }}">{{ $category->nom_categoria }}</option>
                             @endforeach
                         </select>
                     </div>
-                    <div class="col-md-4">
+                    <div class="form-group col-md-6">
                         <label for="nom" class="form-label">Sucursal</label>
-                        <select class="form-select" name="nom_sucursal">
+                        <select class="form-control" name="nom_sucursal">
                             <option selected>Sucursal</option>
                             @foreach ($offices as $office)
                                 <option value="{{ $office->id }}">{{ $office->nom_sucursal }}</option>
                             @endforeach
                         </select>
                     </div>   
-                    <div class="col-md-4" >
+                    <div class="form-group col-md-6" >
                         <label for="precio" class="form-label">Cantidad</label>
                         <input type="precio" class="form-control" id="precio" name="cantidad">
                     </div>
-                    <div class="form-floating">
-                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" name="descripcion" style="height: 100px"></textarea>
+                    <div class="form-group col-md-12">
                         <label for="floatingTextarea2">Descripción</label>
+                        <textarea class="form-control" placeholder="Agrega un comentario al producto" id="floatingTextarea2" name="descripcion" style="height: 100px"></textarea>
                     </div>
-                    <button type="submit" class="btn btn-warning">Agregar</button>
+                    <button type="submit" class="btn btn-block btn-success">Agregar producto</button> 
+
                 </form>
             </div>
         </div>
--- resources/views/pages/products/edit.blade.php
+++ resources/views/pages/products/edit.blade.php
@@ -4,29 +4,41 @@
 
 @section('content')
     @section('titulo', 'Editar')
-    <div class="position-absolute top-50 start-50 translate-middle">
+
+
+    <h1 class="text-center">Editar Producto</h1>
+    <br>
+
+    <div class="row justify-content-center">
+
         <div class="card" style="width: 40rem;" >
+
             <div class="card-body">
                 <form action="{{ url('/productos/actualizar') }}" method="POST" class="row g-2">
                     @csrf
                     <input type="hidden" name="codigo" value="{{ $product->codigo }}">
-                    <div class="col-md-6">
+
+                    <div class="form-group col-md-6">
                         <label for="name" class="form-label">Nombre Producto</label>
                         <input type="text" class="form-control" id="name" name="name" value="{{ $product->name }}">
                     </div>
-                    <div class="col-md-6" >
-                        <label for="precio_venta" class="form-label">precio venta</label>
+
+                    <div class="form-group col-md-6" >
+                        <label for="precio_venta" class="form-label">Precio venta</label>
                         <input type="text" class="form-control" id="precio_venta" name="precio_venta" value="{{ $product->precio_venta }}">
                     </div>
-                    <div class="form-floating">
-                        <textarea class="form-control" name="descripcion" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px">{{ $product->descripcion }}</textarea>
+
+                    <div class="form-group col-md-12">
                         <label for="floatingTextarea2">Descripción</label>
+                        <textarea class="form-control" name="descripcion" placeholder="Agrega un comentario al producto" id="floatingTextarea2" style="height: 100px">{{ $product->descripcion }}</textarea>
                     </div>
-                    <button type="submit" class="btn btn-warning">Actualizar</button>
+                    
+                    <button type="submit" class="btn btn-block btn-warning ">Actualizar</button>
+                    
                 </form>
             </div>
         </div>
-    </div>
-    
+    </div> 
+     
 
 @endsection
